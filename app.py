217258f25918484f5cb499fe1c3406fa56d8163d
Midhun R Nair <midhun.rnair011@gmail.com>
2020-06-13 13:16:44 +0530

    Added Column Account Balance and Message

--- app.py
+++ app.py
@@ -11,16 +11,21 @@ class Customer:
     id = db.Column(db.Integer(), primary_key=True)
     ssnid = db.Column(db.Integer(), unique=True, nullable=False)
     accountId = db.Column(db.Integer(), nullable=False)
+    accountBalance = db.Column(db.Integer(), nullable=False)
     account_type = db.Column(db.String(1), nullable=False)
     status = db.Column(db.Text, nullable=False)
     last_updated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    message = db.Column(db.Text, nullable=False)
 
-    def __init__(self, ssnid, accountId, account_type, status, last_updated):
+    def __init__(self, ssnid, accountId, account_type, status, last_updated, message):
         self.ssnid = ssnid
         self.accountId = accountId
+        self.accountBalance = accountBalance
         self.account_type = account_type
         self.status = status
         self.last_updated = last_updated
+        self.message = message
+        
 
     def __repr__(self):
         return "Customer id: "+str(self.id)

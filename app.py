dc6b18a5e6acbc684e99fc349dcfb0fa35753237
Midhun R Nair <midhun.rnair011@gmail.com>
2020-06-13 13:04:54 +0530

    Updated DB Schema in App.py
    Changed the Customer table by adding:
     Account ID, Account type, status, message, Last updated :+1:

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
-# class Customer:
-#     id = db.Column(db.Integer(), primary_key=True)
-#     ssnid = db.Column(db.Integer(), unique=True, nullable=False)
-#     cname = db.Column(db.String(30), nullable=False)
-#     cage = db.Column(db.Integer(), nullable=False)
-#     caddress = db.Column(db.Text, nullable=False)
-#     ccity = db.Column(db.Text, nullable=False)
-#     cstate = db.Column(db.Text, nullable=False)
+class Customer:
+    id = db.Column(db.Integer(), primary_key=True)
+    ssnid = db.Column(db.Integer(), unique=True, nullable=False)
+    accountId = db.Column(db.Integer(), nullable=False)
+    account_type = db.Column(db.String(1), nullable=False)
+    status = db.Column(db.Text, nullable=False)
+    last_updated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-#     def __init__(self, ssnid, cname, cage, caddress, ccity, cstate):
-#         self.ssnid = ssnid
-#         self.cname = cname
-#         self.cage = cage
-#         self.caddress = caddress
-#         self.ccity = ccity
-#         self.cstate = cstate
+    def __init__(self, ssnid, accountId, account_type, status, last_updated):
+        self.ssnid = ssnid
+        self.accountId = accountId
+        self.account_type = account_type
+        self.status = status
+        self.last_updated = last_updated
 
-#     def __repr__(self):
-#         return "Customer id: "+str(self.id)+" with Customer name: "+self.cname
+    def __repr__(self):
+        return "Customer id: "+str(self.id)
 
 
 @app.route('/')

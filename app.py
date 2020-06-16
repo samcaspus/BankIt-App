21c5618f48a3210e2aefed8836e63ae77844293c
sarvesh.agrawal <sarvesh.agrawal@triquetra.in>
2020-06-17 04:08:25 +0530

    Create_Customer Update_Customer Delete_Customer Add_Account

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from cid_generator import randN
 
 # ###########################
 # Database Configuration
@@ -24,7 +25,6 @@ class Customer(db.Model):
     last_updated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     message = db.Column(db.Text, nullable=False)
 
-
     def __init__(self, cid, ssnid, accountId, accountBalance, account_type, status, message):
         self.cid = cid
         self.ssnid = ssnid
@@ -34,6 +34,29 @@ class Customer(db.Model):
         self.status = status
         self.message = message
         
+
+    def __repr__(self):
+        return "Customer id: "+str(self.cid)
+
+
+class CustomerDetails(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    cid = db.Column(db.Integer(), nullable=False)
+    ssnid = db.Column(db.Integer(), nullable=False)
+    customer_name = db.Column(db.String(10), nullable=False)
+    age = db.Column(db.Integer(), nullable=False)
+    address = db.Column(db.String(10), nullable=False)
+    state = db.Column(db.String(10), nullable=False)
+    city = db.Column(db.String(10), nullable=False)
+
+    def __init__(self, cid, ssnid, customer_name, age, address, state, city):
+        self.cid = cid
+        self.ssnid = ssnid
+        self.customer_name = customer_name
+        self.age = age
+        self.address = address
+        self.state = state
+        self.city = city
 
     def __repr__(self):
         return "Customer id: "+str(self.cid)
@@ -91,7 +114,6 @@ def CustomerSearch():
 @app.route('/AccountSearch', methods=['GET', 'POST'])
 def AccountSearch():
     if request.method == 'POST':
-       
         if 'accid' in request.form:
             accid = request.form['accid']
             results = db.session.query(Customer).filter(Customer.accountId == accid)
@@ -102,6 +124,90 @@ def AccountSearch():
             return render_template('account-Search.html', result=results)
     else:   
         return render_template('account-Search.html')
+
+@app.route('/createcustomer', methods=['GET', 'POST'])
+def Createcustomer():
+    cid = int(randN())
+    print(cid)
+    # rendered = render_template('create-customer.html', cid=cid) 
+    if request.method == 'POST':
+        if 'ssnid' in request.form:
+            ssnid = int(request.form['ssnid'])
+            customer_name = request.form['customer_name']
+            age = int(request.form['age'])
+            address = request.form['address']
+            state = request.form['state']
+            city = request.form['city']
+            
+            # inserting data
+            db.create_all()
+            db.session.add(CustomerDetails(cid=cid, ssnid=ssnid, customer_name=customer_name, age=age, address=address, state=state, city=city))
+            db.session.commit()
+            return render_template('create-customer.html')
+        else:
+            return render_template('create-customer.html') 
+    else:
+        return render_template('create-customer.html') 
+
+
+@app.route('/addaccount', methods =['GET','POST'])
+def Addaccount():  
+    if request.method == 'POST':
+        if 'cid' in request.form:
+            cid = int(request.form['cid'])
+            account_type = request.form['account_type']
+            accountBalance = request.form['accountBalance']
+
+            results = db.session.query(CustomerDetails).filter(CustomerDetails.cid==cid)
+            for row in results:
+                ssnid = row.ssnid
+            accountId = int(randN())
+            db.session.add(Customer(cid=cid, ssnid=ssnid, accountId=accountId, accountBalance=accountBalance, account_type=account_type, status='Pending Approval', message='Just Created'))
+            db.session.commit()
+            return render_template('create-account.html')
+        else:
+            return render_template('create-account.html')
+
+        
+    else:
+        return render_template('create-account.html')
+
+@app.route('/updatecustomer', methods =['GET','POST'])
+def UpdateCustomer(): 
+    if request.method == 'POST':
+        if 'cid' in request.form:
+            cid = int(request.form['cid'])
+            customer_name = request.form['customer_name']
+            address = request.form['address']
+            age = int(request.form['age'])
+
+            results = db.session.query(CustomerDetails).filter(CustomerDetails.cid == cid)
+            print(results[0])
+            for row in results:
+                row.customer_name = customer_name
+                row.address = address
+                row.age = age
+            db.session.commit()
+            return render_template('update-customer.html')
+        else:
+            return render_template('update-customer.html')
+    else:
+        return render_template('update-customer.html')
+
+
+@app.route('/deletecustomer', methods =['GET','POST'])
+def DeleteCustomer(): 
+    if request.method == 'POST':
+        if 'cid' in request.form:
+            cid = int(request.form['cid'])
+            db.session.query(Customer).filter(Customer.cid == cid).delete()
+            db.session.query(CustomerDetails).filter(CustomerDetails.cid == cid).delete()
+            db.session.commit()
+            return render_template('delete-customer.html')
+        else:
+            return render_template('delete-customer.html')
+    else:
+        return render_template('delete-customer.html')
 
 
 if __name__ == '__main__':
